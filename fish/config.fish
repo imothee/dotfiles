a221fe298729c03dc4e44c1f68e4060d5ad9ff99
Tim Marks <t@imothee.com>
2026-06-30 20:39:10 -0700

    chore(fish): drop nix + antigravity cruft, add .local/bin, mise, lms

--- fish/config.fish
+++ fish/config.fish
@@ -1,12 +1,14 @@
 # Add Paths
 fish_add_path "/opt/homebrew/bin/"
+fish_add_path "$HOME/.local/bin/"
 
 # Set env variables
 set -U fish_greeting ""
 
-if test -f /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-    source /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.fish
-end
-
 # Init starship
 starship init fish | source
+# Added by LM Studio CLI (lms)
+set -gx PATH $PATH /Users/tim/.lmstudio/bin
+# End of LM Studio CLI section
+
+mise activate fish | source
